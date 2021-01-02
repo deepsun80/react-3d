6c346bd11b8fe7f2bf509caf48b4d50b4562c313
Sandeep Chandran <schandran2@apple.com>
2021-01-02 10:09:15 -0600

    building

--- src/App.js
+++ src/App.js
@@ -10,27 +10,40 @@ function Boxes() {
   const ref = useRef();
 
   useFrame(() => {
+    // ref.current.rotation.x += 0.002;
     ref.current.rotation.y += 0.002;
 
-    let i = 0;
-    for (let x = 0; x < 5; x++) {
-      for (let y = 0; y < 5; y++) {
-        for (let z = 0; z < 5; z++) {
-          const id = i++;
-          tempObject.position.set(x * 2, y * 2, z * 2);
+    // let i = 0;
+    // for (let x = 0; x < 5; x++) {
+    //   for (let y = 0; y < 5; y++) {
+    //     for (let z = 0; z < 5; z++) {
+    //       const id = i++;
+    //       tempObject.position.set(x * 2, y * 2, z * 2);
+    //       tempObject.updateMatrix();
+    //       ref.current.setMatrixAt(id, tempObject.matrix);
+    //     }
+    //   }
+    // }
+    for (let i = 0; i < 10000; i ++) {
+          // const id = i++;
+          tempObject.position.set(Math.random() * 30 - 15, Math.random() * 30 - 15, Math.random() * 30 - 15);
           tempObject.updateMatrix();
-          ref.current.setMatrixAt(id, tempObject.matrix);
+          ref.current.setMatrixAt(i, tempObject.matrix);
         }
-      }
-    }
   });
 
   return (
     <instancedMesh ref={ref} args={[null, null, 1000]}>
-      <boxBufferGeometry attach="geometry" args={[0.7, 0.7, 0.7]} />
+      <boxBufferGeometry attach="geometry" args={[0.2, 0.2, 2.14]} />
       <meshPhongMaterial attach="material" color="red" />
     </instancedMesh>
   );
+}
+
+function Spehere() {
+  return (
+    
+  )
 }
 
 function Scene() {
