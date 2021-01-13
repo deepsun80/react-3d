74a2132f2e60018bed3f7f86dcda0004b8b8a093
Sandeep Chandran <schandran2@apple.com>
2021-01-13 15:33:15 -0600

    opacity added

--- src/components/EnemiesDestroyed.js
+++ src/components/EnemiesDestroyed.js
@@ -1,16 +1,22 @@
+import { useState } from 'react';
 import { useRecoilValue } from "recoil";
 import { enemyDestroyedState } from "../gameState";
 
 // Manages Drawing enemies that currently exist in state
 function EnemiesDestroyed() {
+ const [opacity, setOpacity] = useState(1.0);
  const enemiesDestroyed = useRecoilValue(enemyDestroyedState);
+
+ setTimeout(() => {
+   setOpacity(0.0);
+ }, 10000);
  
  return (
    <group>
      {enemiesDestroyed.map((enemy, id) => (
        <mesh position={[enemy.x, enemy.y, enemy.z]} key={`${id}`}>
          <sphereBufferGeometry attach="geometry" args={[2, 20,20]} />
-         <meshStandardMaterial attach="material" color="red" wireframe />
+         <meshStandardMaterial opacity={0} attach="material" color="red" wireframe />
        </mesh>
      ))}
    </group>
